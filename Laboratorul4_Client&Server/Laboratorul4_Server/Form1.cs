1f2ccae3e03dd25acfc74b11c8c5aaae0acc09da
katy198 <55131487+katy198@users.noreply.github.com>
2020-04-28 17:45:13 +0300

    update

--- Laboratorul4_Client&Server/Laboratorul4_Server/Form1.cs
+++ Laboratorul4_Client&Server/Laboratorul4_Server/Form1.cs
@@ -8,7 +8,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
-namespace Laboratorul_Server
+namespace Laboratorul4_Server
 {
     public partial class Form1 : Form
     {
